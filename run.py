9f858a70a42f2b5cb8abce94b00c81e6fb59ebac
vi-dsouza <vitoriasteffane5@gmail.com>
2026-07-16 14:28:12 -0300

    feat: novas mudanças no run

--- run.py
+++ run.py
@@ -5,31 +5,31 @@ from app.database import get_connection
 from flask_cors import CORS
 from app.auth.routes import auth_bp
 
+app = create_app()
+
+CORS(
+    app,
+    resources={r"/*": {"origins": ["http://localhost:3000", "https://seu-front-no-render.com"]}},
+    supports_credentials=True
+)
+
+@app.route('/config_blog/<path:filename>')
+def serve_config_blog(filename):
+    path_root = os.path.join(os.getcwd(), 'config_blog')
+    return send_from_directory(path_root, filename)
+
 def start_server():
     try:
-        app = create_app()
-        
-        CORS(
-            app,
-            resources={r"/*": {"origins": "http://localhost:3000"}},
-            supports_credentials=True
-        )
-
-        @app.route('/config_blog/<path:filename>')
-        def serve_config_blog(filename):
-            path_root = os.path.join(os.getcwd(), 'config_blog')
-            return send_from_directory(path_root, filename)
-
         host = os.getenv("FLASK_HOST", "127.0.0.1")
         port = int(os.getenv("FLASK_PORT", 5000))
         debug = os.getenv("FLASK_DEBUG", "True") == "True"
 
-        print("\n🚀 Servidor iniciado com sucesso!")
+        print("\n🚀 Servidor local iniciado com sucesso!")
         print(f"URL: http://{host}:{port}")
 
         app.run(host=host, port=port, debug=debug)
     except Exception as e:
-        print(f"❌ Erro: {e}")
+        print(f"❌ Erro ao iniciar localmente: {e}")
 
 if __name__ == "__main__":
     start_server()
